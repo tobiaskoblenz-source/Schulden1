3b67f93fe18c889540fcb6d86c811d51a487b237
tobiaskoblenz-source <tobiaskoblenz@googlemail.com>
2026-05-30 18:37:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'dark-v29-right-panel-wider';
+const VERSION = 'dark-v30-approved-layout';
 const CACHE_STATIC = `schulden-manager-static-${VERSION}`;
 const CACHE_RUNTIME = `schulden-manager-runtime-${VERSION}`;
 const APP_SHELL = [
