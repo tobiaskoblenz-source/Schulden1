e8796de598ee0301adb580395ed0bd36643b272f
tobiaskoblenz-source <tobiaskoblenz@googlemail.com>
2026-05-30 19:21:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'dark-v34-aktenzeichen-linked';
+const VERSION = 'dark-v35-save-fix-aktenzeichen-linked';
 const CACHE_STATIC = `schulden-manager-static-${VERSION}`;
 const CACHE_RUNTIME = `schulden-manager-runtime-${VERSION}`;
 const APP_SHELL = [
