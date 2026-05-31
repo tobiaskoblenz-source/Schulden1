7ba6e6d8bdca9375c81e60d2cd783d5b1f2498d6
tobiaskoblenz-source <tobiaskoblenz@googlemail.com>
2026-05-31 02:29:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'dark-v50-schnellaktionen-buttons-entfernt';
+const VERSION = 'dark-v51-alle-buttons-nebeneinander';
 const CACHE_STATIC = `schulden-manager-static-${VERSION}`;
 const CACHE_RUNTIME = `schulden-manager-runtime-${VERSION}`;
 const APP_SHELL = [
