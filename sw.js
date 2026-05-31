fe14723d4d809cfd10aa80dc606da67ee3cc1dbb
tobiaskoblenz-source <tobiaskoblenz@googlemail.com>
2026-05-31 03:21:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'dark-v52-google-buttons-neben-pdf';
+const VERSION = 'dark-v53-desktop-akte-backup-polish';
 const CACHE_STATIC = `schulden-manager-static-${VERSION}`;
 const CACHE_RUNTIME = `schulden-manager-runtime-${VERSION}`;
 const APP_SHELL = [
