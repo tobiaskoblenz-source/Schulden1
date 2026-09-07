87f543afc2cfbe7f83183161ebbbdcd07ed11aa5
tobiaskoblenz-source <tobiaskoblenz@googlemail.com>
2026-09-07 23:19:32 +0200

    Refresh PWA cache for compact creditor cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v167-compact-mobile-home';
+const VERSION = 'v168-compact-mobile-creditors';
 const CACHE_STATIC = `schulden-manager-static-${VERSION}`;
 const CACHE_RUNTIME = `schulden-manager-runtime-${VERSION}`;
 const APP_SHELL = [
