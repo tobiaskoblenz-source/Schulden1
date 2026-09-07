57cf65df890f55253193c97ee1ed61c15c992e3b
tobiaskoblenz-source <tobiaskoblenz@googlemail.com>
2026-09-07 23:27:16 +0200

    Refresh PWA cache after reverting narrow cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v169-narrow-mobile-creditors';
+const VERSION = 'v170-revert-narrow-creditors';
 const CACHE_STATIC = `schulden-manager-static-${VERSION}`;
 const CACHE_RUNTIME = `schulden-manager-runtime-${VERSION}`;
 const APP_SHELL = [
